0088fbaa4c19a15b953b1ab0b5a06b914a3b5464
Alexis Rico <sferadev@gmail.com>
2017-10-16 14:43:57 +0200

    UserData: Add writeToDisk
    Login functionality already works

--- src/main/java/data/UserControllerJSON.java
+++ src/main/java/data/UserControllerJSON.java
@@ -6,12 +6,20 @@ import domain.model.User;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class UserControllerJSON implements UserController {
     private static final String FILE_LOCATION = "data/users.json";
+
     private Map<String, User> mUsers;
+
+    private Gson gson = new Gson();
 
     private static UserControllerJSON mInstance = new UserControllerJSON();
 
@@ -41,16 +49,33 @@ public class UserControllerJSON implements UserController {
     public boolean insert(User user) {
         if (exists(user.getName())) return false;
         mUsers.put(user.getName(), user);
+        updateDisk();
         return true;
     }
 
     private Map<String, User> openFile() {
         try {
-            Gson gson = new Gson();
             return gson.fromJson(new Scanner(new File(FILE_LOCATION)).useDelimiter("\\Z").next(),
                     (Type) new HashMap<>());
         } catch (FileNotFoundException e) {
+            Path pathToFile = Paths.get(FILE_LOCATION);
+            try {
+                Files.createDirectories(pathToFile.getParent());
+                Files.createFile(pathToFile);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
             return new HashMap<>();
+        }
+    }
+
+    private void updateDisk() {
+        try {
+            FileWriter writer = new FileWriter(new File(FILE_LOCATION), false);
+            writer.write(gson.toJson(mUsers));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
