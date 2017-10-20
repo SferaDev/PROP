444e933970a1cc85cda8bb929d0f6a87b7317b01
Alexis Rico <sferadev@gmail.com>
2017-10-20 11:25:45 +0200

    Remove shitty wip

--- src/main/java/domain/model/Row.java
+++ src/main/java/domain/model/Row.java
@@ -6,13 +6,5 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Row<E extends Peg> extends ArrayList<E> {
-    public void order() {
-        sort(new Comparator<E>() {
-            @Override
-            public int compare(E e1, E e2) {
-
-                return 0;
-            }
-        });
-    }
+    // TODO
 }
