2c46ac763b6e309ccb2905894d31d6fbeb34397d
oriolborrell <oriolborrellroig@gmail.com>
2017-10-20 08:38:46 +0200

    intento de genetic

--- src/main/java/domain/model/player/computer/GeneticComputer.java
+++ src/main/java/domain/model/player/computer/GeneticComputer.java
@@ -24,6 +24,8 @@ public class GeneticComputer extends ComputerPlayer {
     private ArrayList<Row<ColorPeg>> feasibleCodes = new ArrayList<>();
     private ArrayList<Row<ColorPeg>> gameGuesses = new ArrayList<>();
 
+    private int parentPos = 0;
+
     public GeneticComputer(Role role, int pegs, int colors, int turns) {
         super(role);
 
@@ -38,7 +40,7 @@ public class GeneticComputer extends ComputerPlayer {
 
     @Override
     public Row<ColorPeg> makerGuess(int pegs, int colors) {
-        makerCorrectGuess = randomRow(pegs, colors);
+        makerCorrectGuess = breakerGuess(pegs, colors);
         return makerCorrectGuess;
     }
 
@@ -69,15 +71,16 @@ public class GeneticComputer extends ComputerPlayer {
         sortFeasibleByFitness(fitness, population);
         while (feasibleCodes.isEmpty()) {
             while (generation <= GENERATION_SIZE && feasibleCodes.size() <= FEASIBLE_CODES_MAX) {
-                // evolvePopulation
+                evolvePopulation(pegs, colors);
                 calculateFitness();
                 sortFeasibleByFitness(fitness, population);
-                // addToFeasible
+                addToFeasibleCodes();
                 generation++;
             }
         }
         Row<ColorPeg> guess = feasibleCodes.get((int) (Math.random() * feasibleCodes.size()));
         gameGuesses.add(guess);
+        ++currentTurn;
         return guess;
     }
 
@@ -111,7 +114,7 @@ public class GeneticComputer extends ComputerPlayer {
             int x = 0;
             int y = 0;
             for (int j = 0; j < gameGuesses.size(); j++) {
-                Row<ControlPeg> compare = ComputerPlayer.compareGuess(population[i], gameGuesses.get(j));
+                Row<ControlPeg> compare = compareGuess(population[i], gameGuesses.get(j));
                 int black = 0;
                 int white = 0;
                 for (ControlPeg peg : compare) {
@@ -166,31 +169,162 @@ public class GeneticComputer extends ComputerPlayer {
         pop[b] = tmp;
     }
 
-    /**private void evolvePopulation(int pegs) {
-        Row[] newPopulation = new Row[POPULATION_SIZE];
+    private void evolvePopulation(int pegs, int colors) {/**/
+
+        Row<ColorPeg>[] newPopulation = new Row[POPULATION_SIZE];
         for (int i = 0; i < POPULATION_SIZE; i++) {
             newPopulation[i] = new Row<ColorPeg>(pegs);
         }
         for (int i = 0; i < POPULATION_SIZE; i += 2) {
             if ((int) (Math.random() * 2) == 0) {
-                xOver1(newPopulation, i, i + 1);
+                xOver1(newPopulation, i, i + 1, pegs);
             } else {
-                xOver2(newPopulation, i, i + 1);
+                xOver2(newPopulation, i, i + 1, pegs);
             }
         }
 
         for (int i = 0; i < POPULATION_SIZE; i++) {
             if ((int) (Math.random() * 100) < 3) {
-                mutation(newPopulation, i);
+                mutation(newPopulation, i, pegs, colors);
             } else if ((int) (Math.random() * 100) < 3) {
-                permutation(newPopulation, i);
+                permutation(newPopulation, i, pegs);
             } else if ((int) (Math.random() * 100) < 2) {
-                inversion(newPopulation, i);
+                inversion(newPopulation, i, pegs);
             }
         }
 
-        doubleToRnd(newPopulation);
+        doubleToRnd(newPopulation, pegs, colors);
 
         population = newPopulation;
-    }**/
+    }
+
+    private void addToFeasibleCodes() {
+        outer:
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            for (int j = 0; j < currentTurn; j++) {
+                Row<ControlPeg> compare = compareGuess(population[i], gameGuesses.get(j));
+                int black = 0;
+                int white = 0;
+                for (ControlPeg peg : compare) {
+                    if (peg.getType() == ControlPeg.Type.BLACK) black += 1;
+                    if (peg.getType() == ControlPeg.Type.WHITE) white += 1;
+                }
+
+                if (black != blacks[j] || white != whites[j]) {
+                    continue outer;
+                }
+            }
+        }
+    }
+
+    private void xOver1(Row<ColorPeg>[] newPopulation, int child1Pos, int child2Pos, int pegs) {
+        int mother = getParentPos();
+        int father = getParentPos();
+        int sep = ((int) (Math.random() * pegs)) + 1;
+
+        for (int j = 0; j < pegs; j++) {
+            if (j <= sep) {
+                newPopulation[child1Pos].setColorAtPos(j,
+                        population[mother].get(j).getColor());
+                newPopulation[child2Pos].setColorAtPos(j,
+                        population[father].get(j).getColor());
+            } else {
+                newPopulation[child1Pos].setColorAtPos(j,
+                        population[father].get(j).getColor());
+                newPopulation[child2Pos].setColorAtPos(j,
+                        population[mother].get(j).getColor());
+            }
+        }
+    }
+
+    private void xOver2(Row[] newPopulation, int child1Pos, int child2Pos, int pegs) {
+        int mother = getParentPos();
+        int father = getParentPos();
+        int sep1;
+        int sep2;
+
+        sep1 = ((int) (Math.random() * pegs)) + 1;
+        sep2 = ((int) (Math.random() * pegs)) + 1;
+
+        if (sep1 > sep2) {
+            int temp = sep1;
+            sep1 = sep2;
+            sep2 = temp;
+        }
+
+        for (int i = 0; i < pegs; i++) {
+            if (i <= sep1 || i > sep2) {
+                newPopulation[child1Pos].setColorAtPos(i,
+                        population[mother].get(i).getColor());
+                newPopulation[child2Pos].setColorAtPos(i,
+                        population[father].get(i).getColor());
+            } else {
+                newPopulation[child1Pos].setColorAtPos(i,
+                        population[father].get(i).getColor());
+                newPopulation[child2Pos].setColorAtPos(i,
+                        population[mother].get(i).getColor());
+            }
+        }
+
+    }
+
+    private int getParentPos() {
+        parentPos += (int) (Math.random() * 7);
+        if (parentPos < POPULATION_SIZE / 5) {
+            return parentPos;
+        } else {
+            parentPos = 0;
+        }
+        return parentPos;
+    }
+
+    private void mutation(Row[] newPopulation, int popPos, int pegs, int colors) {
+        newPopulation[popPos].setColorAtPos((int) (Math.random() * pegs),
+                (int) (Math.random() * colors));
+    }
+
+    private void permutation(Row[] newPopulation, int popPos, int pegs) {
+        int pos1 = (int) (Math.random() * pegs);
+        int pos2 = (int) (Math.random() * pegs);
+        int tmp = newPopulation[popPos].getColorAtPos(pos1);
+        newPopulation[popPos].setColorAtPos(pos1,
+                newPopulation[popPos].getColorAtPos(pos2));
+        newPopulation[popPos].setColorAtPos(pos2, tmp);
+    }
+
+    private void inversion(Row[] newPopulation, int popPos, int pegs) {
+        int pos1 = (int) (Math.random() * pegs);
+        int pos2 = (int) (Math.random() * pegs);
+
+        if (pos2 < pos1) {
+            int tmp = pos2;
+            pos2 = pos1;
+            pos1 = tmp;
+        }
+
+        for (int i = 0; i < (pos2 - pos1)/2; i++) {
+            int tmp = newPopulation[popPos].getColorAtPos(pos1 + i);
+            newPopulation[popPos].setColorAtPos(pos1 + i,
+                    newPopulation[popPos].getColorAtPos(pos2 - i));
+            newPopulation[popPos].setColorAtPos(pos2 - i, tmp);
+        }
+    }
+
+    private void doubleToRnd(Row[] newPopulation, int pegs, int colors) {
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            if (lookForSame(newPopulation, i)) {
+                newPopulation[i] = randomRow(pegs, colors);
+            }
+        }
+    }
+
+    private boolean lookForSame(Row[] newPopulation, int popPos) {
+        for (int i = 0; i < POPULATION_SIZE; i++) {
+            if (population[popPos].equals(newPopulation[popPos])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
