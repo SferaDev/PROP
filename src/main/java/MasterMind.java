0a6249038b6b7d2f60ff9b61316daa85cab2de62
Alexis Rico <sferadev@gmail.com>
2017-11-09 20:53:28 +0100

    Mastermind: Add some wip code

--- src/main/java/MasterMind.java
+++ src/main/java/MasterMind.java
@@ -3,8 +3,25 @@ import presentation.TerminalApp;
 public class MasterMind {
 
     public static void main(String[] args) {
-        TerminalApp application = new TerminalApp();
-        application.startApplication();
+        if (args.length > 0 && args[0].contains("terminal")) {
+            TerminalApp application = new TerminalApp();
+            application.startApplication();
+        } else {
+            // TODO: GFX
+            TerminalApp application = new TerminalApp();
+            application.startApplication();
+        }
+
+        /** See TODO: See this!!! (Alexis)
+         try {
+         Process p = Runtime.getRuntime().exec(new String[] {
+         "bash", "-c", "tput lines 2> /dev/tty" });
+         BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+         // Can be null
+         System.out.println(in.readLine());
+         } catch (IOException e) {
+         e.printStackTrace();
+         }**/
     }
 
 }
