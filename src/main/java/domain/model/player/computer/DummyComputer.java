601d578f102aca619b84476f14535c6597db7bd1
Alexis Rico <sferadev@gmail.com>
2017-11-14 09:51:26 +0100

    JavaDoc DummyComputer

--- src/main/java/domain/model/player/computer/DummyComputer.java
+++ src/main/java/domain/model/player/computer/DummyComputer.java
@@ -20,16 +20,30 @@ public class DummyComputer extends ComputerPlayer implements java.io.Serializabl
         super(role);
     }
 
+    /**
+     * Return the name of the algorithm
+     */
     @Override
     public String getName() {
         return "Dummy";
     }
 
+    /**
+     * Generates the next attempt. The guess is generated randomly.
+     * @param pegs   is the number of pegs accepted in the game
+     * @param colors is the number of colors accepted in the game
+     * @return the combination that is going to be tried
+     */
     @Override
     public ColorRow breakerGuess(int pegs, int colors) {
         return randomRow(pegs, colors);
     }
 
+    /**
+     * Recives the correction of the previous attempt. As each attempts is independent from the previous,
+     * we do nothing.
+     * @param control is the number of black and white pegs of the last attempt
+     */
     @Override
     public void receiveControl(ControlRow control) {
         // I don't care, I love it!
@@ -40,6 +54,11 @@ public class DummyComputer extends ComputerPlayer implements java.io.Serializabl
         // Should never happen
     }
 
+    /**
+     * Generates the correction of one guess
+     * @param guess is the attempt that will be corrected
+     * @return the number of blacks and whites of that guess
+     */
     @Override
     public ControlRow scoreGuess(ColorRow guess) {
         ControlRow score = super.scoreGuess(guess);
@@ -53,6 +72,12 @@ public class DummyComputer extends ComputerPlayer implements java.io.Serializabl
         return score;
     }
 
+    /**
+     *Generates a random secret code
+     * @param pegs is the number of pegs in the combination
+     * @param colors is the number of different possible colors in a combination
+     * @return the secret code
+     */
     @Override
     public ColorRow makerGuess(int pegs, int colors) {
         ColorRow correctGuess = super.makerGuess(pegs, colors);
