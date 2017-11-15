a3538d6ba78be53bcdf11efd94c02758d4ad2a6e
oriolborrell <oriolborrellroig@gmail.com>
2017-11-16 00:22:31 +0100

    Driver ColorRow

--- src/domain/test/drivers/DriverColorRow.java
+++ src/domain/test/drivers/DriverColorRow.java
@@ -1,4 +1,36 @@
 package domain.test.drivers;
 
+import domain.model.exceptions.CommandInterruptException;
+import domain.model.exceptions.FinishGameException;
+import domain.model.row.ColorRow;
+import presentation.model.TerminalInputOutput;
+import presentation.utils.TerminalMenuBuilder;
+
 public class DriverColorRow {
+    private static TerminalMenuBuilder terminalMenuBuilder = new TerminalMenuBuilder();
+    private static TerminalInputOutput terminalInputOutput = new TerminalInputOutput();
+
+
+    public static void main(String args[]){
+        terminalMenuBuilder.addTitle("Menu DriverColorRow:");
+        terminalMenuBuilder.addOption("Probar creadora", DriverColorRow::case1);
+        terminalMenuBuilder.addOption("Sortir",terminalMenuBuilder::finishExecution);
+        terminalMenuBuilder.execute();
+    }
+
+
+    private static void case1() {
+        int[] cint = null;
+        try {
+             cint = terminalInputOutput.inputColorRow(4, 4);
+        } catch (FinishGameException e) {
+            e.printStackTrace();
+        } catch (CommandInterruptException e) {
+            e.printStackTrace();
+        }
+        ColorRow c = new ColorRow(cint);
+        terminalInputOutput.outputColorRow(c.toString());
+    }
+
 }
+
