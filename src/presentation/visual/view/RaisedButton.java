9d2ccaa5601b65183c21f9400553ec2f0c0c526b
Alexis Rico <sferadev@gmail.com>
2017-12-11 14:03:21 +0100

    Add some stuff

---
+++ src/presentation/visual/view/RaisedButton.java
@@ -0,0 +1,19 @@
+package presentation.visual.view;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.geometry.Insets;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.layout.HBox;
+
+public class RaisedButton extends JFXButton {
+    public RaisedButton(String text) {
+        setButtonType(ButtonType.RAISED);
+        setContentDisplay(ContentDisplay.CENTER);
+        setMnemonicParsing(false);
+        setStyle("-fx-font-size:14px; -fx-background-color:WHITE;");
+
+        HBox.setMargin(this, new Insets(10, 10, 10, 10));
+
+        setText(text);
+    }
+}
--- src/presentation/visual/view/SelectionPeg.java
+++ src/presentation/visual/view/SelectionPeg.java
@@ -1,19 +1,22 @@
 package presentation.visual.view;
 
 import com.jfoenix.controls.JFXNodesList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
 
 public class SelectionPeg extends JFXNodesList {
     private ColorPeg mainPeg;
 
-    public SelectionPeg(String mainColor, ArrayList<String> availableColors) {
+    // PRE: availableColors.size() > 0
+    public SelectionPeg(ArrayList<String> availableColors) {
         setRotate(180);
         setSpacing(10);
         setAlignment(Pos.CENTER);
 
-        mainPeg = new ColorPeg(mainColor);
+        mainPeg = new ColorPeg(availableColors.get(0));
         addAnimatedNode(mainPeg);
 
         for (String color : availableColors) {
