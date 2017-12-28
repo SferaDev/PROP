768691deef78ecc6a1b33f2d02d00d7b24287060
Alexis Rico <sferadev@gmail.com>
2017-12-28 21:01:50 +0100

    Nebula: Reuse views

--- src/presentation/visual/controller/NebulaViewController.java
+++ src/presentation/visual/controller/NebulaViewController.java
@@ -1,13 +1,13 @@
 package presentation.visual.controller;
 
 import com.jfoenix.controls.JFXButton;
-import domain.controller.DomainController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import presentation.visual.utils.ColorManager;
 import presentation.visual.utils.LocaleUtils;
 import presentation.visual.view.*;
 import presentation.visual.view.components.ColorRow;
@@ -23,9 +23,14 @@ public class NebulaViewController implements Initializable {
     @FXML
     private BorderPane mainContent;
 
-    private GameView gameView;
     private BoardPane boardPane;
     private boolean isPlaying = false;
+
+    private GameView currentGameView;
+    private NewGameView newGameView = new NewGameView();
+    private StatsView statsView = new StatsView();
+    private UserOptionsView userOptionsView = new UserOptionsView();
+    private HelpView helpView = new HelpView();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -43,18 +48,17 @@ public class NebulaViewController implements Initializable {
     private void onNavigationDrawerClick(String item) {
         switch (item) {
             case "PLAY":
-                if (isPlaying) mainContent.setCenter(gameView);
-                else mainContent.setCenter(new NewGameView());
+                if (isPlaying) mainContent.setCenter(currentGameView);
+                else mainContent.setCenter(newGameView);
                 break;
             case "STATS":
-                mainContent.setCenter(new StatsView());
+                mainContent.setCenter(statsView);
                 break;
             case "USER":
-                mainContent.setCenter(new UserOptionsView());
+                mainContent.setCenter(userOptionsView);
                 break;
             case "HELP":
-                DomainController.getInstance().getGameController().startNewGame("alexis", "FiveGuessComputer", "MAKER", 5, 6, 12);
-                mainContent.setCenter(new HelpView());
+                mainContent.setCenter(helpView);
                 break;
             case "LOG_OUT":
                 System.exit(0); // TODO: Should close and launch LoginView
@@ -65,33 +69,38 @@ public class NebulaViewController implements Initializable {
     public void startGame(int size) {
         isPlaying = true;
         boardPane = new BoardPane(size);
-        gameView = new GameView(boardPane);
-        mainContent.setCenter(gameView);
+        currentGameView = new GameView(boardPane);
+        mainContent.setCenter(currentGameView);
     }
 
     public void finishGame() {
         isPlaying = false;
+        mainContent.setCenter(newGameView);
     }
 
     public void addControlLastTurn(int blacks, int whites) {
         boardPane.addControlRow(new ControlRow(boardPane.getSize(), blacks, whites));
-        gameView.scrollDown();
+        currentGameView.scrollDown();
     }
 
     public void addTurn(ColorRow colorRow) {
         boardPane.addColorRow(colorRow);
-        gameView.scrollDown();
+        currentGameView.scrollDown();
     }
 
     public void addControlPane(ControlInput pane) {
-        gameView.addControlPane(pane);
+        currentGameView.addControlPane(pane);
+    }
+
+    public void removeControlPane() {
+        currentGameView.removeControlPane();
     }
 
     private JFXButton createDrawerButton(String textId) {
         JFXButton button = new JFXButton();
         button.setPrefWidth(100);
         button.setText(LocaleUtils.getInstance().getString(textId));
-        button.setTextFill(Color.web("eceff1")); // TODO: Colors
+        button.setTextFill(Color.web(ColorManager.getColor("WHITE"))); // TODO: Colors
         button.setFont(new Font(20));
         button.setOnMouseClicked(event -> onNavigationDrawerClick(textId));
         return button;
