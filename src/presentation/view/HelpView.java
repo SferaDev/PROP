adc4f16ef3c7825216abd1457c7f4e14bd05dbf3
oriolborrell <oriolborrellroig@gmail.com>
2018-01-06 19:59:32 +0100

    HelpView loads proba.html

--- src/presentation/view/HelpView.java
+++ src/presentation/view/HelpView.java
@@ -1,6 +1,33 @@
 package presentation.view;
 
-import javafx.scene.layout.AnchorPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-public class HelpView extends AnchorPane {
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class HelpView extends VBox {
+    public HelpView() {
+        setSpacing(10);
+        setPadding(new Insets(5));
+        setAlignment(Pos.TOP_CENTER);
+
+        WebView webView = new WebView();
+        WebEngine webEngine = webView.getEngine();
+        String path = new File("").getAbsolutePath();
+        path += "\\docs\\proba.html";
+        try {
+            File file = new File(path);
+            URL url = file.toURI().toURL();
+            webEngine.load(url.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        getChildren().addAll(webView);
+    }
 }
