ef4cb235f959e3e936666e1e21df394341fa1126
oriolborrell <oriolborrellroig@gmail.com>
2018-01-07 11:03:44 +0100

    JavaDoc4

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -8,6 +8,10 @@ import presentation.VisualMastermind;
  * @author Alexis Rico Carreto
  */
 public class MainApplication {
+    /**
+     * Starts the Mastermind application
+     * @param args are the arguments entered by terminal
+     */
     public static void main(String[] args) {
         // If run with --terminal open a terminal menu, otherwise launch default Visual app
         if (args.length > 0 && (args[0].equals("-t") || args[0].equals("--terminal"))) {
